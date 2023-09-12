51525dd78f9c6c41dae659771e0b3067f9020fbd
Jesus Yepes <jesus@goltratec.com>
2023-09-12 22:17:52 +0200

    yup

---
+++ App.tsx
@@ -0,0 +1,126 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useRef, useState} from 'react';
+import {SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View,} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import SignatureScreen from "react-native-signature-canvas";
+
+
+const imgWidth = 300;
+const imgHeight = 200;
+const style = `.m-signature-pad {box-shadow: none; border: none; } 
+              .m-signature-pad--body {border: none;}
+              .m-signature-pad--footer {display: block; margin: 0px;}
+              body,html {
+              width: ${imgWidth}px; height: ${imgHeight}px;}`;
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const [signatureData, setSignatureData] = useState('');
+
+  const ref = useRef();
+
+  // Called after ref.current.readSignature() reads a non-empty base64 string
+  const handleOK = (signature) => {
+    console.log('handleOk')
+    setSignatureData(signature)
+    // console.log(signature);
+  };
+
+  // Called after ref.current.readSignature() reads an empty string
+  const handleEmpty = () => {
+    console.log("Empty");
+  };
+
+  // Called after ref.current.clearSignature()
+  const handleClear = () => {
+    console.log("clear success!");
+  };
+
+  // Called after end of stroke
+  const handleEnd = () => {
+    ref.current.readSignature();
+  };
+
+  // Called after ref.current.getData()
+  const handleData = (data) => {
+    setSignatureData(data)
+    console.log('handleData')
+    // console.log(data);
+  };
+
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+
+        <View style={{ width: imgWidth, height: imgHeight }}>
+
+        <SignatureScreen
+          ref={ref}
+          onEnd={handleEnd}
+          onOK={handleOK}
+          onEmpty={handleEmpty}
+          onClear={handleClear}
+          onGetData={handleData}
+          autoClear={true}
+
+          bgWidth={imgWidth}
+          bgHeight={imgHeight}
+          webStyle={style}
+        />
+        </View>
+
+        <View>
+          <Text>Signature data</Text>
+          {signatureData.length ? (
+              <Text>{signatureData}</Text>
+          ) : (
+            <Text>No data, draw something</Text>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+
+
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
